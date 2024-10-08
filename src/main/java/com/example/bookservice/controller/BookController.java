43a9399fa9f5b3e29beb973d887c5590def122d5
manoejKr <287642@ust.com>
2024-10-08 17:19:45 +0530

    delete by id api created

--- src/main/java/com/example/bookservice/controller/BookController.java
+++ src/main/java/com/example/bookservice/controller/BookController.java
@@ -3,6 +3,7 @@ package com.example.bookservice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,12 @@ public class BookController {
     public List<Books> getAllBooks()
     {
         return repositoryBook.findAll();
+    }
+
+    @DeleteMapping("/books/{id}")
+    public void deleteBookById(@PathVariable("id") long id)
+    {
+        repositoryBook.deleteById(id);
     }
 
     
